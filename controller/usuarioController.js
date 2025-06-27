900f053f91e3f5a354e3a97c5ca1706ad6f0179e
jupitters <sampaiobale@gmail.com>
2025-06-27 19:31:04 -0300

    edit user

--- controller/usuarioController.js
+++ controller/usuarioController.js
@@ -25,6 +25,36 @@ const cadastrarUsuario = (req, res) => {
     }
 }
 
+const editUsuario = (req, res) => {
+    const usuario = req.session.usuario;
+    const currentPass = req.body.oldPassword;
+    const newPass = req.body.newPassword == "" ? currentPass : req.body.newPassword;
+
+    if(currentPass != req.session.usuario.senha){
+        let erro = true;
+        res.render('profile.html', {erro, usuario});
+        return
+    }
+
+    let userUpdated = {
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: newPass
+    }
+   
+    Usuario.findOne({
+        where: {
+            id: req.params.id,
+        }
+    }).then((usuario) => {
+        req.session.usuario.senha = newPass;
+        usuario.update(userUpdated);
+    }).then(()=>{
+        res.redirect('/home');
+    })
+}
+
 module.exports = {
     cadastrarUsuario,
+    editUsuario
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controller/usuarioController');
+const authController = require('../controller/authController');
 
 router.post('/api/cadastro', usuarioController.cadastrarUsuario);
+router.post('/api/edit-profile/:id', authController.verificarAutenticacao, usuarioController.editUsuario)
 
 module.exports = router;
